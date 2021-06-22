6c643c01ac3b9740c067bc1cbffaa3c259c8d4fc
mobir <mobir@laymancraft.com>
2021-06-23 00:35:31 +0530

    First cut

---
+++ jpa-locks/src/main/java/com/laymancraft/jpa/locks/JpaLocksApplication.java
@@ -0,0 +1,13 @@
+package com.laymancraft.jpa.locks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JpaLocksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JpaLocksApplication.class, args);
+	}
+
+}
---
+++ jpa-locks/src/test/java/com/laymancraft/jpa/locks/JpaLocksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.laymancraft.jpa.locks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JpaLocksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
